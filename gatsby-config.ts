95cbe01704a4c84dc98aebaa6c483ce48428e675
Jacob Bodera <jacobbodera@Jacobs-MacBook-Air.local>
2022-05-23 20:03:46 -0400

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Jacob Bodera`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-theme-ui"]
+};
+
+export default config;
